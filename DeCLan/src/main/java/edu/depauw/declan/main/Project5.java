ff6dc3f9d8891e9d6b08215b18a6e728bd3bd01b
Brian Howard <bhoward@depauw.edu>
2020-10-27 22:50:23 -0400

    Updated Project 5 (full) model.

---
+++ DeCLan/src/main/java/edu/depauw/declan/main/Project5.java
@@ -0,0 +1,76 @@
+package edu.depauw.declan.main;
+
+import java.util.Properties;
+
+import edu.depauw.declan.common.ErrorLog;
+import edu.depauw.declan.common.ParseException;
+import edu.depauw.declan.common.Parser;
+import edu.depauw.declan.common.ast.ASTVisitor;
+import edu.depauw.declan.common.ast.Program;
+import edu.depauw.declan.model.ReferenceChecker;
+
+/**
+ * Main class for Project 5 -- Full typechecker for DeCLan (Fall 2020).
+ * Parse a program, then check for type errors.
+ * 
+ * @author bhoward
+ */
+public class Project5 {
+	public static void main(String[] args) {
+		String demoSource =
+				  "(* Declare some constants and a global variable *)\n"
+				+ "CONST six = 6; seven = 7;\n"
+				+ "VAR answer : INTEGER;\n"
+				+ "(* Define a function *)\n"
+				+ "PROCEDURE gcd(a: INTEGER; b: INTEGER): INTEGER;\n"
+				+ "  VAR c : INTEGER;\n"
+				+ "  BEGIN\n"
+				+ "    IF b = 0 THEN c := a\n"
+				+ "    ELSE c := gcd(b, a DIV b)\n"
+				+ "    END;\n"
+				+ "    RETURN c\n"
+				+ "  END gcd;\n"
+				+ "(* Define a proper procedure *)\n"
+				+ "PROCEDURE Display(VAR answer: INTEGER; a, b: INTEGER);\n"
+				+ "  VAR i : INTEGER;\n"
+				+ "  BEGIN\n"
+				+ "    FOR i := a TO b BY -1 DO\n"
+				+ "      PrintInt(answer); PrintLn;\n"
+				+ "      WHILE answer > i DO answer := answer - 1\n"
+				+ "      ELSIF answer < i DO answer := answer + 1\n"
+				+ "      END\n"
+				+ "    END\n"
+				+ "  END Display;\n"
+				+ "(*********** Main Program ***********)\n"
+				+ "BEGIN\n"
+				+ "  answer := six * seven * gcd(six, seven);\n"
+				+ "  PrintString(\"The answer is \");\n"
+				+ "  Display(answer, seven, six);\n"
+				+ "  PrintInt(answer); PrintLn;\n"
+				+ "END.\n";
+
+		Properties props = new Properties();
+		props.setProperty("useModelLexer", "true");
+		props.setProperty("useModelParser", "true");
+		props.setProperty("useFullParser", "true");
+		props.setProperty("sourceFile", "");
+		props.setProperty("demoSource", demoSource);
+
+		Config config = new Config(args, props);
+
+		try (Parser parser = config.getParser()) {
+			Program program = parser.parseProgram();
+			// TODO replace the ReferenceChecker with your own visitor object
+			ASTVisitor checker = new ReferenceChecker(config.getErrorLog());
+			program.accept(checker);
+		} catch (ParseException pe) {
+			System.err.println(pe.getMessage());
+		}
+
+		for (ErrorLog.LogItem item : config.getErrorLog()) {
+			System.err.println(item);
+		}
+
+		System.out.println("DONE");
+	}
+}
