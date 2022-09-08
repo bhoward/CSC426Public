95e21d09c53c99328f3e1b5e8a51fcdbbe90e0fb
bhoward <bhoward@J278-TS.depauw.edu>
2022-09-08 15:37:40 -0400

    Finished JSON example

--- CraftingInterpreters/src/main/java/com/craftinginterpreters/demo/ch06json/Parser.java
+++ CraftingInterpreters/src/main/java/com/craftinginterpreters/demo/ch06json/Parser.java
@@ -1,15 +1,24 @@
 package com.craftinginterpreters.demo.ch06json;
 
+import static com.craftinginterpreters.demo.ch06json.TokenType.COLON;
+import static com.craftinginterpreters.demo.ch06json.TokenType.COMMA;
 import static com.craftinginterpreters.demo.ch06json.TokenType.EOF;
 import static com.craftinginterpreters.demo.ch06json.TokenType.FALSE;
 import static com.craftinginterpreters.demo.ch06json.TokenType.LEFT_BRACE;
 import static com.craftinginterpreters.demo.ch06json.TokenType.LEFT_BRACKET;
 import static com.craftinginterpreters.demo.ch06json.TokenType.NULL;
 import static com.craftinginterpreters.demo.ch06json.TokenType.NUMBER;
+import static com.craftinginterpreters.demo.ch06json.TokenType.RIGHT_BRACE;
+import static com.craftinginterpreters.demo.ch06json.TokenType.RIGHT_BRACKET;
 import static com.craftinginterpreters.demo.ch06json.TokenType.STRING;
 import static com.craftinginterpreters.demo.ch06json.TokenType.TRUE;
 
+import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class Parser {
     private static class ParseError extends RuntimeException {
@@ -34,9 +43,9 @@ public class Parser {
 
     private Object value() {
         if (match(LEFT_BRACE)) {
-//            return object();
+            return object();
         } else if (match(LEFT_BRACKET)) {
-//            return array();
+            return array();
         } else if (match(STRING)) {
             return previous().literal;
         } else if (match(NUMBER)) {
@@ -49,6 +58,50 @@ public class Parser {
             return null;
         }
         throw error(peek(), "Expected value.");
+    }
+
+    private Object array() {
+        List<Object> result = new ArrayList<>();
+
+        if (!match(RIGHT_BRACKET)) {
+            Object element = value();
+            result.add(element);
+
+            while (match(COMMA)) {
+                element = value();
+                result.add(element);
+            }
+
+            consume(RIGHT_BRACKET, "Expected closing ']'.");
+        }
+
+        return result;
+    }
+
+    private Object object() {
+        Map<String, Object> result = new HashMap<>();
+
+        if (!match(RIGHT_BRACE)) {
+            var member = member();
+            result.put(member.getKey(), member.getValue());
+
+            while (match(COMMA)) {
+                member = member();
+                result.put(member.getKey(), member.getValue());
+            }
+
+            consume(RIGHT_BRACE, "Expected closing '}'.");
+        }
+
+        return result;
+    }
+
+    private Entry<String, Object> member() {
+        Token key = consume(STRING, "Expected string key.");
+        consume(COLON, "Expected colon separating key from value.");
+        Object value = value();
+
+        return new SimpleEntry<String, Object>((String) key.literal, value);
     }
 
     private boolean match(TokenType... types) {
